a2327ab93afab2fb7f25090baa0f8b557e9b1794
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-02 16:30:08 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {Component, Fragment} from 'react';
+import Navbar from "./components/Layout/Navbar/Navbar";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Index from './components/Index/Index'
+import Lyrics from './components/Tracks/Lyrics/Lyrics'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Fragment>
+          <Navbar/>
+          <div className="container">
+            <Switch>
+              <Route path='/lyrics/track/:id' component={Lyrics}/>
+              <Route path='/' component={Index}/> 
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
