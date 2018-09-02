f0a9e605930a16eeb97049891f458a09ea4c6e57
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-02 16:28:53 +0530

    spinner component added

---
+++ src/components/Layout/Spinner/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Spinner.css'
+
+const Spinner = () => (
+    <div className="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+)
+
+export default Spinner
