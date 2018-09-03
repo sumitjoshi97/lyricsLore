326f61f15e522ae147ab59e8959403fbfc952e2d
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-03 15:53:07 +0530

    refactored

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Navbar = () => {
-  return (
-    <nav className="navbar navbar-dark bg-dark mb-5">
-        <span className="navbar-brand mb-0 h1 mx-auto">Lyrics Lore</span>
-    </nav>
-  )
-}
+const Navbar = () => (
+  <nav className="navbar navbar-dark bg-blue mb-5 gd">
+    <span className="navbar-brand mb-0 h2 mx-auto">Lyrics Lore</span>
+  </nav>
+)
 
 export default Navbar
