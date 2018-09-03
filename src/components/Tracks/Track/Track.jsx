dd7a78cbf52aa218e7c65056b84830a5fa973a3b
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-03 15:53:16 +0530

    track title as heading

--- src/components/Tracks/Track/Track.jsx
+++ src/components/Tracks/Track/Track.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+
 const Track = ({track}) => {
     return (
         <div className="col-md-6">
-            <div className="carfd mb-4 shadow-sm">
+            <div className="card mb-4 shadow">
                 <div className="card-body">
-                    <h5>{track.artist_name}</h5>
+                    <h5 className="font-weight-bold mb-3">{track.track_name}</h5>
                     <p className="card-text">
                         <strong>
-                            <i className="fa fa-play"></i>
-                            Track</strong>: {track.track_name}
+                            <i className="fa fa-user"></i>{' '}
+                            Artist</strong>: {track.artist_name}
                         <br/>
                         <strong>
-                            <i className="fa fa-compact-disc"></i>
+                            <i className="fa fa-compact-disc"></i>{' '}
                             Album</strong>: {track.album_name}
-                    </p>
-                    <Link to={`/lyrics/track/${track.track_id}`} className="btn btn-dark btn-block">
-                        <i className="fa fa-chevron-right"></i>
-                        View Lyrics
-                    </Link>
+                    </p>             
                 </div>
+                <Link to={`/lyrics/track/${track.track_id}`} className="btn-block btn-card">
+                        <i className="fa fa-chevron-right"></i>
+                        {' '}View Lyrics
+                </Link>
             </div>
         </div>
     )
