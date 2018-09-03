40da98d5c4ee5ee83102fdbad53fec60ba0fe44d
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-03 15:54:17 +0530

    lyrics text formatted

--- src/components/Tracks/Lyrics/Lyrics.jsx
+++ src/components/Tracks/Lyrics/Lyrics.jsx
@@ -1,8 +1,9 @@
-import React, {Component, Fragment} from 'react'
+import React, { Component, Fragment } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Moment from 'react-moment'
 import Spinner from '../../Layout/Spinner/Spinner'
+
 export class Lyrics extends Component {
   state = {
     track: {},
@@ -13,33 +14,43 @@ export class Lyrics extends Component {
     axios
       .get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_LYRICS}`)
       .then(res => {
-        this.setState({lyrics: res.data.message.body.lyrics}, ()=>console.log(this.state.lyrics))
+        this.setState({ lyrics: res.data.message.body.lyrics })
         return axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_LYRICS}`)
       })
       .then(res => {
-        this.setState({track: res.data.message.body.track}, () => console.log(this.state.track))
+        this.setState({ track: res.data.message.body.track })
       })
       .catch(err => console.log(err))
   }
-  
-  render() { 
-    const {track, lyrics} = this.state
+
+  render() {
+    const { track, lyrics } = this.state
     if (track === undefined || lyrics === undefined || Object.keys(track).length === 0 || Object.keys(lyrics).length === 0) {
-      return <Spinner/>
+      return <Spinner />
     } else {
+
+      const lyricsSet = JSON.parse(JSON.stringify(lyrics.lyrics_body))
+      const lyricsText = lyricsSet.split('\\n').map((item, i) => {
+        return <p key={i}>{item}</p>
+      })
+
       return (
         <Fragment>
-          <Link to="/" className="btn btn-dak btn-sm mb-4">Go Back</Link>
-          <div className="card">
-            <h5 className="card-header">
-              {track.track_name} by <span className="text-secondary">{track.artist_name}</span>
-            </h5>
+          <Link to="/" className="btn btn-dak btn-sm mb-4"><i className="fa fa-arrow-left"></i> Go Back</Link>
+          <h4>Lyrics</h4>
+          <h1 className="font-weight-bold">
+            {track.track_name}
+          </h1>
+          <h3 className="text-primary font-weight-bold">
+            {track.artist_name}
+          </h3>
+          <div className="card shadow">
             <div className="card-body">
-              <p className="card-text">{lyrics.lyrics_body}</p>
+              {lyricsText}
             </div>
           </div>
 
-          <ul className="list-group mt-3">
+          <ul className="list-group my-5 shadow">
             <li className="list-group-item">
               <strong>Album ID</strong> : {track.album_id}
             </li>
