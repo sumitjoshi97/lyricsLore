73989290cc398ae41b0930853edfa1ef419c4eb6
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-03 16:26:53 +0530

    styles imported

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {Provider} from './context'
+import { Provider } from './context';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 const app = (
     <Provider>
-        <App/>
+        <App />
     </Provider>
 )
 ReactDOM.render(app, document.getElementById('root'));
